ba7f22462f1ccdf859c245258f1dfc00a98b5085
Tim Lee <timlee104@gmail.com>
2018-03-16 03:41:31 -0500

    Added users, coins, ICOs to admin utility

--- cryptocounter/admin.py
+++ cryptocounter/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import Coin, Ico
+
 # Register your models here.
+admin.site.register(Coin)
+admin.site.register(Ico)
--- cryptocounter/models.py
+++ cryptocounter/models.py
@@ -3,24 +3,27 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Coin(models.Model):
-    coin_id = models.IntegerField(default=0)
-    coin_name = models.CharField(max_length=200)
-    ticker = models.CharField(max_length=10)
+    coin_id = models.IntegerField(primary_key=True, default=0)
+    coin_name = models.CharField(unique=True, max_length=200)
+    ticker = models.CharField(unique=True, max_length=10)
     block_chain = models.CharField(max_length=10)
-    search_terms = models.TextField(max_lenght=500)
+    search_terms = models.TextField(max_length=500)
 
 class Ico(models.Model):
-    ico_id = models.IntegerField(default=0)
-    ico_name = models.CharField(max_length=200)
+    ico_id = models.IntegerField(primary_key=True, default=0)
+    ico_name = models.CharField(unique=True, max_length=200)
     start = models.DateField()
     end = models.DateField()
     description = models.TextField()
-    search_terms = models.TextField(max_lenght=500)
+    search_terms = models.TextField(max_length=500)
 
 class Price(models.Model):
     coin_id = models.ForeignKey(Coin, on_delete=models.CASCADE)
     date = models.DateField()
-    price = models.DecimalField(max_digits=20, decimal_places=2)
+    price = models.DecimalField(max_digits=20, decimal_places=2, default=0)
+    circ_supply = models.IntegerField(default=0)
+    percent_change = models.DecimalField(max_digits=5, decimal_places=2, default=0)
+    market_cap = models.DecimalField(max_digits=20, decimal_places=2, default=0)
 
 class SocialCoin(models.Model):
     coin_id = models.ForeignKey(Coin, on_delete=models.CASCADE)
