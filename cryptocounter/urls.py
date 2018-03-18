5ee25061847840dc85da9b05d34d5f70c8575f3e
Tim Lee <timlee104@gmail.com>
2018-03-18 04:39:30 -0500

    Incorporated the HTML files into the site and defined URLs

---
+++ cryptocounter/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.market, name='market'),
+    path('market', views.market, name='market'),
+    path('ico', views.ico, name='ico'),
+    path('socialtrends', views.socialTrends, name='socialtrends'),
+    path('watchlist', views.watchlist, name='watchlist'),
+    path('login', views.login, name='login'),
+    path('register', views.register, name='register'),
+    path('header.html', views.header, name='header'),
+    path('footer.html', views.footer, name='footer'),
+]
--- cryptocounter/views.py
+++ cryptocounter/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
 # Create your views here.
+def market(request):
+    return render(request, 'cryptocounter/market.html')
+
+def ico(request):
+    return render(request, 'cryptocounter/ico.html')
+
+def socialTrends(request):
+    return render(request, 'cryptocounter/trends.html')
+
+def watchlist(request):
+    return render(request, 'cryptocounter/watchlist.html')
+
+def login(request):
+    return render(request, 'cryptocounter/index.html')
+
+def register(request):
+    return render(request, 'cryptocounter/register.html')
+
+def header(request):
+    return render(request, 'cryptocounter/header.html')
+
+def footer(request):
+    return render(request, 'cryptocounter/footer.html')
