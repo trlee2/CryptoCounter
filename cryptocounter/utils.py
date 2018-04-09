10bb3bd8c6f0601ba7904624c36ee781e471267a
Tim Lee <timlee104@gmail.com>
2018-04-09 13:53:41 -0500

    Fixed error handling for when no pricing data for coin exists

--- cryptocounter/utils.py
+++ cryptocounter/utils.py
@@ -8,16 +8,24 @@ def getCurrPrices():
     data = []
     # get all tracked coins
     coins = Coin.objects.all()
+
     # get time range
-    time = datetime.now() - timedelta(hours=24)
-    # get each coin price
-    for coin in coins:
-        # get only the most recent price
-        pData = Price.objects.filter(coin_id=coin.coin_id).get(date__gte=time)
-        # create table row
-        temp = {'coin_name':coin.coin_name, 'ticker':coin.ticker, 'price':pData.price,
-        'circ_supply':pData.circ_supply, 'percent_change':pData.percent_change, 'market_cap':pData.market_cap}
-        data.append(temp)
+    #time = datetime.now() - timedelta(hours=24)
+
+    # check if there are any coins in the DB
+    if coins.exists():
+        # get each coin price
+        for coin in coins:
+            # get only the most recent price if there is one
+            try:
+                pData = Price.objects.filter(coin_id=coin.coin_id).order_by('-date')
+                pData = pData[0]
+                # create table row
+                temp = {'coin_name':coin.coin_name, 'ticker':coin.ticker, 'price':pData.price,
+                'circ_supply':pData.circ_supply, 'percent_change':pData.percent_change, 'market_cap':pData.market_cap}
+                data.append(temp)
+            except:
+                print('No pricing data for coin found')
     return data
 
 # return all ICO data for ico page
@@ -29,13 +37,26 @@ def getIcoInfo():
 # return all data on an individual coin
 def getCoinDetails(cname):
     # get coin info
-    coin = Coin.objects.get(coin_name=cname)
+    try:
+        coin = Coin.objects.get(coin_name=cname)
+    except:
+        print('Coin does not exist in database')
+
     # get the coin's current price
-    time = datetime.now() - timedelta(hours=24)
-    coinPrice = Price.objects.filter(coin_id=coin.coin_id).get(date__gte=time)
+    #time = datetime.now() - timedelta(hours=24)
+
+    # try to get the pricing data for coin if there's any
+    try:
+        coinPrice = Price.objects.filter(coin_id=coin.coin_id).order_by('-date')
+        coinPrice = coinPrice[0]
+    except:
+        print('No pricing data for coin found')
+        return []
+
     # TODO: get coin's social trends
 
     # send back the relevant info
     coinData = {'coin_name':coin.coin_name, 'ticker':coin.ticker, 'price':coinPrice.price,
     'circ_supply':coinPrice.circ_supply, 'percent_change':coinPrice.percent_change, 'market_cap':coinPrice.market_cap}
+
     return coinData
