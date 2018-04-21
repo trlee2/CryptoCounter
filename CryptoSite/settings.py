819c28dc9e704becf5666777dd0ab4e55cfb15dc
Tim Lee <timlee104@gmail.com>
2018-04-20 20:28:20 -0500

    Preparing for Heroku deployment

--- CryptoSite/settings.py
+++ CryptoSite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '8t8c9!cxd50k!r_=fua@4t6f8#g6dy8&=2_1x@kf6+u+mexzoo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
@@ -77,12 +78,12 @@ WSGI_APPLICATION = 'CryptoSite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'cryptoDB',
         'USER': 'ccadmin',
         'PASSWORD': '1',
         'HOST': 'localhost',
-        'PORT': '5432',
+        'PORT': '',
     }
 }
 
@@ -123,3 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
---
+++ Procfile
@@ -0,0 +1,2 @@
+web: gunicorn CryptoSite.wsgi --log-file -
+
